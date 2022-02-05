64e520556805d536ccde9b548a6dc8195b324394
sihyunjojo <si4018@naver.com>
2022-02-05 12:09:09 +0900

    완성 ver 1.0

--- js/background.js
+++ js/background.js
@@ -1,7 +1,4 @@
 const images = [
-    "abutterfly-4848340.jpg",
-    "astronomy-1867616.jpg",
-    "cat-3678858.jpg",
     "flowers-2300523.jpg",
     "plants-4572694.jpg",
     "still-life-3126536.jpg",
@@ -9,8 +6,19 @@ const images = [
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-//img태그를 생성시켜서 변수로
+const bgImage = document.createElement("img");
+
+bgImage.className = "background";
+bgImage.src = `img/${chosenImage}`;
+
+ const bgCover = document.createElement("div");
+
+bgCover.className = "background-cover";
+
+document.body.appendChild(bgCover);
+document.body.appendChild(bgImage);
+/* //img태그를 생성시켜서 변수로
 const bgImage = `url('img/${chosenImage}')`;
 
 document.body.style.backgroundImage = bgImage;
-//appendChild는 body에 html을 추가하는 것이다.
+//appendChild는 body에 html을 추가하는 것이다. */
